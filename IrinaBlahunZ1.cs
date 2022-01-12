e87847f40fd23fe6cfa19642740f6c6e990ec242
IrinaBlahun <94350381+IrinaBlahun@users.noreply.github.com>
2022-01-12 14:59:29 +0300

    Add files via upload

---
+++ IrinaBlahunZ1.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace IrinaBlahunZ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число N");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n > 7) 
+            {
+                Console.WriteLine("Привет");
+            } 
+            else
+            {
+                Console.WriteLine("Вы не достойны приветсвия)");
+            }
+        }
+                    
+    }
+}
