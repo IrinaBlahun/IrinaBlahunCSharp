d2c125cf6d69ccf4e6eb000f27a2d4a44aca8e77
IrinaBlahun <94350381+IrinaBlahun@users.noreply.github.com>
2022-01-14 09:42:00 +0300

    Update IrinaBlahunZ1.cs

--- IrinaBlahunZ1.cs
+++ IrinaBlahunZ1.cs
@@ -1,23 +1,31 @@
 using System;
 
+
 namespace IrinaBlahunZ1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите число N");
-            int n = int.Parse(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("Введите число N");
+                int n = int.Parse(Console.ReadLine());
+                if (n > 7)
+                {
+                    Console.WriteLine("Привет");
+                }
+                else 
+                {
+                    Console.WriteLine("Вы не достойны приветсвия)");          
+                }           
+            }
 
-            if (n > 7) 
+            catch (FormatException)
             {
-                Console.WriteLine("Привет");
-            } 
-            else
-            {
-                Console.WriteLine("Вы не достойны приветсвия)");
+                Console.WriteLine("Ошибка формата");
             }
         }
-                    
+
     }
 }
